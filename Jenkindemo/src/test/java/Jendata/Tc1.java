b4360930ba1cfac990ec5ca38492eb3f66a74c93
Palavelli Sreenu <palavellisreenu7@gmail.com>
2023-05-13 15:22:19 +0530

    Jenkins 1st data

---
+++ Jenkindemo/src/test/java/Jendata/Tc1.java
@@ -0,0 +1,12 @@
+package Jendata;
+
+import org.testng.annotations.Test;
+
+public class Tc1
+{
+@Test
+public void Testcase1()
+{
+	System.out.println("Tc1 excuted");
+}
+}
---
+++ Jenkindemo/src/test/java/Jendata/Tc2.java
@@ -0,0 +1,13 @@
+
+package Jendata;
+
+import org.testng.annotations.Test;
+
+public class Tc2
+{
+	@Test
+	public void Testcase2()
+	{
+		System.out.println("Tc1 excuted");
+	}
+}
